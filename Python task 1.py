4978c48534fa8b35c922e7375f3f4bd48af41c5d
ayoblvck <60554621+ayoblvck@users.noreply.github.com>
2020-03-24 19:32:25 +0100

    Update Python task 1.py

--- Python task 1.py
+++ Python task 1.py
@@ -1,4 +1,4 @@
 import math
-radius = float(input('please enter the radius of the circle: '))
+radius = float(input('Please enter the radius of the circle: '))
 area = math.pi * radius * radius
 print ("Area of the circle is:%.2f= " %area)
